23b7418c6b8511e962d32668cc67fd2c38e17158
Commodoreprime <35617951+Commodoreprime@users.noreply.github.com>
2024-01-31 20:10:26 -0900

    Added script file as-is

---
+++ godot.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+from os import environ as env
+# from pprint import pprint
+from pathlib import Path
+from sys import argv
+import os
+import re
+
+try:
+    data_path_parent: str = env["XDG_DATA_HOME"]
+except KeyError:
+    data_path_parent = f"{os.path.expanduser('~')}/.local/share"
+
+GODOT_BINS_DIRECTORY = Path(f"{data_path_parent}/godot-bin")
+
+def godot_bin_indexer(bin_search_path: str|Path) -> list:
+    return_list = []
+    for directory in bin_search_path.iterdir():
+        file_path: Path = None
+        if directory.is_dir():
+            for subdir in directory.iterdir():
+                if subdir.is_file():
+                    file_path = subdir
+        elif directory.is_file():
+            file_path = directory
+        
+        bin_basename = os.path.basename(file_path)
+        
+        godot_version = re.findall(r'v[0-9].+-', bin_basename)[0][1:-1]
+        godot_release_class = re.findall(r'-\w+_', bin_basename)[0][1:-1]
+        godot_is_mono = godot_release_class.count("mono") >= 1
+        
+        return_list.append({
+            "bin_path": file_path,
+            "version": godot_version,
+            "release_class": godot_release_class.split('_')[0],
+            "is_mono": godot_is_mono
+            })
+    return return_list
+
+godots_index: list = sorted(godot_bin_indexer(GODOT_BINS_DIRECTORY), key=lambda k: k["version"])
+
+# pprint(godots_index, indent=2)
+
+def reverse_search_value_dict(source_list: list, target_key: str, target_value: str) -> list:
+    """ Iterate over a list of dicts and given a key and value, return list of index positions that point to matching dict items.
+    target_value can be any amount of precision, given a use-case.
+    The returned list is reversed so the last found appears first, second-to-last appears second, etc...
+    """
+    return_list = []
+    for i, item in enumerate(source_list):
+        if item[target_key].startswith(target_value) == True:
+            return_list.append(i)
+    return_list.reverse()
+    return return_list
+
+def gather_version(parsing_list: list, get_startidx: bool = False) -> dict:
+    return_object: dict = {}
+    release_class_found: bool = False
+    for i, arg in enumerate(parsing_list):
+        return_object.update({"argv_startidx": i})
+        if arg == "--":
+            return_object["argv_startidx"] += 1
+            break
+        elif arg.replace('.', '').isdigit() == True:
+            if return_object.get("version") == None:
+                return_object.update({"version": ""})
+            return_object["version"] += f"{arg}."
+        elif arg == "mono":
+            return_object.update({"using_mono": True})
+        elif (arg.count("stable") + arg.count("rc") + arg.count("beta") + arg.count("alpha")) == 1 and release_class_found == False:
+            return_object.update({"release_class": arg})
+            release_class_found = True
+        else:
+            break
+    
+    if return_object["argv_startidx"] > -1:
+        return_object["argv_startidx"] += 1
+    elif get_startidx == False or return_object["argv_startidx"] <= -1:
+        return_object["argv_startidx"] = None
+
+    if return_object.get("version") != None:
+        return_object["version"] = return_object["version"].strip('.')
+    
+    return return_object
+
+# Doubles as "default" arguments
+target_godot: dict = {
+    "version": godots_index[len(godots_index) - 1]["version"], # i.e. 4.2
+    "release_class": "stable",
+    "using_mono": False,
+    "argv_startidx": -1
+}
+
+script_name = os.path.basename(argv[0])[5:]
+if len(script_name) != 0 or script_name != ".py":
+    target_godot.update(gather_version(script_name.split('-'), get_startidx=False))
+
+target_godot.update(gather_version(argv[1:], get_startidx=True))
+
+highest_match_idxs = reverse_search_value_dict(godots_index, "version", target_godot["version"])
+
+print(f'{target_godot["argv_startidx"]} "{argv[target_godot["argv_startidx"]:]}", {target_godot["version"]}, {target_godot["release_class"]}, is mono = {target_godot["using_mono"]}')
+
+for idx in highest_match_idxs:
+    item = godots_index[idx]
+    if item["release_class"] == target_godot["release_class"] and \
+       item["is_mono"] == target_godot["using_mono"]:
+            exec_path = item["bin_path"]
+            os.execvp(exec_path, [exec_path, *argv[target_godot["argv_startidx"]:]])
+    # print(f'({idx}): {item}')
+
+print(f"Was unable to locate the Godot binary in {GODOT_BINS_DIRECTORY}")
+
+exit(1)
